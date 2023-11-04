682add5cde64175ea9e48b2949b373a047670aff
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2023-11-04 12:42:47 -0400

    initial message queue API

--- MESSAGE_QUEUE.h
+++ MESSAGE_QUEUE.h
@@ -3,4 +3,16 @@
 
 void MESSAGE_QUEUE_INIT(void);
 
+void PRIORITY_MESSAGE_QUEUE_INIT(void);
+
+bool check_is_queue_active(uint8_t queuetype);
+
+uint8_t register_component_handler_for_messages(void (*func_ptr)(uint8_t), uint8_t* handle);
+
+uint8_t unregister_component_handler_for_messages(uint8_t handle);
+
+uint8_t register_priority_handler_for_messages(void (*func_ptr)(uint8_t), uint8_t* handle);
+
+uint8_t unregister_priority_handler_for_messages(uint8_t handle);
+
 #endif
