84d0fb13bed5488713cbc00862299b65c192b34b
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2023-12-09 15:43:05 -0500

    making it a little more rusty

--- functional_tests/src/main.rs
+++ functional_tests/src/main.rs
@@ -4,9 +4,9 @@
 
 include!("bindings.rs");
 
-fn appendNewTOFReadVal(dat: &[u8;3], size: usize)
+fn appendNewTOFReadVal(dat: &[u8], size: usize)
 {
-    let test_ptr = dat as *const u8; // and a pointer, created from the reference
+    let test_ptr = dat.as_ptr() as *const u8; // and a pointer, created from the reference
     let ret_bool;
     for x in dat
     {
@@ -25,13 +25,13 @@ fn main()
     println!("Beginning running tests.");
     let mut test_data: [u8; 3] = [0; 3];
     test_data[0] = 0x41;
-    appendNewTOFReadVal(&test_data, 1); // a shared reference to this data...
+    appendNewTOFReadVal(&test_data[..1], 1); // a shared reference to this data...
     test_data[0] = 0x03;
-    appendNewTOFReadVal(&test_data, 3);
+    appendNewTOFReadVal(&test_data[..3], 3);
     test_data[0] = 0x00;
-    appendNewTOFReadVal(&test_data, 1);
+    appendNewTOFReadVal(&test_data[..1], 1);
     test_data[0] = 0x08;
-    appendNewTOFReadVal(&test_data, 1);
+    appendNewTOFReadVal(&test_data[..1], 1);
     unsafe
     {
         app_main();
