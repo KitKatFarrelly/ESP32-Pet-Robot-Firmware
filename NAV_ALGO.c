e8abff5f271c101b33dbaf66a80203a204632c36
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2024-09-30 23:33:05 -0400

    commiting outline

--- NAV_ALGO.c
+++ NAV_ALGO.c
@@ -4,8 +4,25 @@
 #include "IMU_SPI.h"
 #include "FLASH_SPI.h"
 
+#define MAX_FEATURES_PER_TOF_ARRAY 10
+
+typedef struct
+{
+    NAV_POINT_T features[MAX_FEATURES_PER_TOF_ARRAY];
+    uint8_t number_of_features;
+} feature_extraction_t;
+
+typedef struct
+{
+    NAV_POINT_T robot_pos;
+    uint8_t submap_x;
+    uint8_t submap_y;
+} robot_position_t;
+
 static callback_handle_t s_nav_tof_handle;
 static callback_handle_t s_nav_imu_handle;
+static robot_position_t s_nav_robot_position;
+static NAV_MAP_T s_nav_map;
 static bool s_is_navigation_enabled = false;
 
 static const char *TAG = "NAV_ALG";
@@ -47,32 +64,32 @@ bool nav_algo_restart_temp_map(void)
     return false;
 }
 
-NAV_MAP_T nav_algo_closest_map_to_temp_map(void)
+NAV_MAP_HANDLER_T nav_algo_closest_map_to_temp_map(void)
 {
     return NULL;
 }
 
-NAV_MAP_T nav_algo_start_writing_map(void)
+NAV_MAP_HANDLER_T nav_algo_start_writing_map(void)
 {
     return NULL;
 }
 
-bool nav_algo_stop_writing_map(NAV_MAP_T map)
+bool nav_algo_stop_writing_map(NAV_MAP_HANDLER_T map)
 {
     return false;
 }
 
-bool nav_algo_save_map(NAV_MAP_T map_to_save)
+bool nav_algo_save_map(NAV_MAP_HANDLER_T map_to_save)
 {
     return false;
 }
 
-bool nav_algo_load_map(NAV_MAP_T map_to_load)
+bool nav_algo_load_map(NAV_MAP_HANDLER_T map_to_load)
 {
     return false;
 }
 
-NAV_MAP_T nav_algo_get_current_map(void)
+NAV_MAP_HANDLER_T nav_algo_get_current_map(void)
 {
     return NULL;
 }
@@ -106,15 +123,70 @@ static void nav_algo_queue_handler(component_handle_t component_type, uint8_t me
 
 static uint8_t nav_algo_convert_adjusted_confidence_value(uint16_t distance, uint8_t confidence)
 {
-    uint16_t square_val = (1000 / 6); //whatever the multiplier should be at 1000 mm
-    uint16_t multiplier = 1 + ((distance * distance) / (square_val * square_val));
-    uint16_t ret_val = multiplier * confidence;
+    float base_mult = 6.0;
+    float square_val = (1000.0 / base_mult); //whatever the multiplier should be at 1000 mm
+    float square_dist = (float) distance;
+    float multiplier = 1.0 + ((square_dist * square_dist) / (square_val * square_val * base_mult));
+    uint16_t ret_val = (uint16_t) (multiplier * (float) confidence);
     return (uint8_t) (ret_val & 0xFF);
+}
+
+//perform feature extraction from tof data
+//generate up to MAX_FEATURES_PER_TOF_ARRAY landmarks
+static feature_extraction_t nav_algo_feature_extraction_from_tof_data(TOF_DATA_t* tof_data)
+{
+    feature_extraction_t return_features_list = {0};
+    //as all features are considered to be planes in this design, features are extracted like so:
+    //1. create a 2x2 convolution of each point to determine vertical and horizontal gradient, starting from top left
+    //2. dfs to find islands of features within the convolution with similar gradients.
+    //3. Combine islands that are likely the same island, then order islands from largest to smallest in terms of number of cells.
+    //4. determine center of mass of each island, xyz dimensions, as well as their orientation vs the robot.
+    return return_features_list;
 }
 
 //read tof map and run error vs existing map to estimate movement
 //also create and adjust objects on each submap
 static void nav_algo_check_tof_array_against_map(TOF_DATA_t* tof_data)
 {
-    
+    //step 1: generate landmarks
+    feature_extraction_t feaures_list = nav_algo_feature_extraction_from_tof_data(tof_data);
+
+    //step 2: find 2 most confident landmarks
+    if(!features_list.number_of_feaures)
+    {
+        //no features were extracted from the array.
+        return;
+    }
+
+    //step 3: determine rotation + translation error of landmark 1 to each existing landmark on submap
+    NAV_POINT_T landmark_error[MAX_POINTS_PER_SUBMAP] = {0};
+    NAV_SUBMAP_T * submap_pointer = &(s_nav_map.map[s_nav_robot_position.submap_x][s_nav_robot_position.submap_y]); //maybe have a getter/setter for this...
+    for(uint8_t iter = 0; iter < MAX_POINTS_PER_SUBMAP; iter++)
+    {
+        if(!submap_pointer->pointCloud[iter].confidence)
+        {
+            //break out once pointclouds start becoming invalid
+            break;
+        }
+        //calculate error of pointCloud[iter] - features_list[0]
+        //if error is sufficiently close to s_nav_robot_position for any given feature in the pointcloud, we have matched the feature.
+        //otherwise, continue with step 3.1
+    }
+
+    //step 3.1: if necessary, determine rotation + translation error of landmark 2 to each existing landmark on submap
+
+    //step 3.2: if necessary, compare each set of errors to build array of potential transforms based on lowest shared error.
+
+    //step 3.3: if necessary, compare each potential transform against 3, 4, 5, etc landmarks until one landmark remains
+
+    //step 4: update location
+
+    //step 5: update submap with map landmark info
 }
+
+//Basic mapping idea:
+//turn any given array into a set of landmarks and translate/rotate array according to estimated robot location
+//calculate distances of one landmark in array to all landmarks in submap and calculate rotational error for each
+//do the same for a second landmark in array.
+//try to find minimum error between two rotational and translation vectors. Use average of the two vectors as array transform onto map
+//update landmarks on map according to new landmarks from array
--- NAV_ALGO.h
+++ NAV_ALGO.h
@@ -1,8 +1,12 @@
 #ifndef H_NAV_ALGO
 #define H_NAV_ALGO
 
-//512 * 8 bytes = 4kB per submap.
-#define MAX_POINTS_PER_SUBMAP 512
+//128 * 8 bytes = 1kB per submap.
+#define MAX_POINTS_PER_SUBMAP 128
+
+#define MAX_SUBMAPS_PER_DIRECTION 10
+
+typedef uint32_t NAV_MAP_HANDLER_T;
 
 //nav_point_t consists of 4 fields position, size, rotation, confidence
 //in total 64 bits of info per point, or 8 bytes.
@@ -20,14 +24,13 @@ typedef struct
 
 typedef struct
 {
-    NAV_POINT_T pointCloud[MAX_POINTS_PER_SUBMAP];
-    int submap_x;
-    int submap_y;
+    NAV_POINT_T pointCloud[MAX_POINTS_PER_SUBMAP]; //Change this to dynamic allocation maybe
 } NAV_SUBMAP_T;
 
 typedef struct
 {
-    uint32_t handler;
+    NAV_SUBMAP_T map[MAX_SUBMAPS_PER_DIRECTION][MAX_SUBMAPS_PER_DIRECTION]; //Change this to dynamic allocation maybe
+    NAV_MAP_HANDLER_T handler;
 } NAV_MAP_T;
 
 typedef struct 
@@ -46,17 +49,17 @@ bool nav_algo_enable_navigation(bool enable);
 
 bool nav_algo_restart_temp_map(void);
 
-NAV_MAP_T nav_algo_closest_map_to_temp_map(void);
+NAV_MAP_HANDLER_T nav_algo_closest_map_to_temp_map(void);
 
-NAV_MAP_T nav_algo_start_writing_map(void);
+NAV_MAP_HANDLER_T nav_algo_start_writing_map(void);
 
-bool nav_algo_stop_writing_map(NAV_MAP_T map);
+bool nav_algo_stop_writing_map(NAV_MAP_HANDLER_T map);
 
-bool nav_algo_save_map(NAV_MAP_T map_to_save);
+bool nav_algo_save_map(NAV_MAP_HANDLER_T map_to_save);
 
-bool nav_algo_load_map(NAV_MAP_T map_to_load);
+bool nav_algo_load_map(NAV_MAP_HANDLER_T map_to_load);
 
-NAV_MAP_T nav_algo_get_current_map(void);
+NAV_MAP_HANDLER_T nav_algo_get_current_map(void);
 
 NAV_CURRENT_POS_T nav_algo_get_current_pos(void);
 
