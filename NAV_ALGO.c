c02609296019aad29316d98a324ad7860128840f
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2024-10-08 01:50:57 -0400

    more feature extraction code

--- NAV_ALGO.c
+++ NAV_ALGO.c
@@ -5,10 +5,13 @@
 #include "FLASH_SPI.h"
 
 #define MAX_FEATURES_PER_TOF_ARRAY 10
+#define MAX_GRADIENT_DIFF_FOR_FEATURE 5
+#define MAX_GRADIENT_MAP_SIZE 8
 
 typedef struct
 {
     NAV_POINT_T features[MAX_FEATURES_PER_TOF_ARRAY];
+    uint8_t number_of_nodes_in_feature[MAX_FEATURES_PER_TOF_ARRAY];
     uint8_t number_of_features;
 } feature_extraction_t;
 
@@ -19,11 +22,27 @@ typedef struct
     uint8_t submap_y;
 } robot_position_t;
 
+typedef struct
+{
+    uint16_t v_diff;
+    uint16_t h_diff;
+    bool visited;
+} gradient_graph_point_t;
+
+
+typedef struct
+{
+    gradient_graph_point_t graph_points[MAX_GRADIENT_MAP_SIZE][MAX_GRADIENT_MAP_SIZE]; //use max size for gradient map
+} gradient_map_t;
+
+
 static callback_handle_t s_nav_tof_handle;
 static callback_handle_t s_nav_imu_handle;
 static robot_position_t s_nav_robot_position;
 static NAV_MAP_T s_nav_map;
 static bool s_is_navigation_enabled = false;
+
+static gradient_map_t gradient_map = {0};
 
 static const char *TAG = "NAV_ALG";
 
@@ -131,14 +150,69 @@ static uint8_t nav_algo_convert_adjusted_confidence_value(uint16_t distance, uin
     return (uint8_t) (ret_val & 0xFF);
 }
 
+//create a new feature via dfs
+static uint8_t nav_algo_create_new_feature_with_dfs(uint8_t v_iter, uint8_t h_iter, NAV_POINT_T *new_feature)
+{
+
+}
+
 //perform feature extraction from tof data
 //generate up to MAX_FEATURES_PER_TOF_ARRAY landmarks
 static feature_extraction_t nav_algo_feature_extraction_from_tof_data(TOF_DATA_t* tof_data)
 {
     feature_extraction_t return_features_list = {0};
+    return_features_list.number_of_features = 0;
     //as all features are considered to be planes in this design, features are extracted like so:
     //1. create a 2x2 convolution of each point to determine vertical and horizontal gradient, starting from top left
+    for(uint8_t v_iter = 0; v_iter < tof_data->horizontal_size - 1; v_iter++)
+    {
+        for(uint8_t h_iter = 0; h_iter < tof_data->horizontal_size - 1; h_iter++)
+        {
+            //negative values are just very high numbers for unsigned integers, which is fine in this case
+            gradient_map.graph_points[v_iter][h_iter].v_diff = (tof_data->depth_pixel_field[v_iter][h_iter] & 0xFFFF) - (tof_data->depth_pixel_field[v_iter + 1][h_iter] & 0xFFFF);
+            gradient_map.graph_points[v_iter][h_iter].h_diff = (tof_data->depth_pixel_field[v_iter][h_iter] & 0xFFFF) - (tof_data->depth_pixel_field[v_iter][h_iter + 1] & 0xFFFF);
+            gradient_map.graph_points[v_iter][h_iter].visited = false;
+        }
+    }
     //2. dfs to find islands of features within the convolution with similar gradients.
+    for(uint8_t v_iter = 0; v_iter < tof_data->horizontal_size - 1; v_iter++)
+    {
+        for(uint8_t h_iter = 0; h_iter < tof_data->horizontal_size - 1; h_iter++)
+        {
+            
+            if(!gradient_map.graph_points[v_iter][h_iter].visited)
+            {
+                //create new feature via dfs
+                NAV_POINT_T new_feature;
+                uint8_t number_of_nodes_in_feature = nav_algo_create_new_feature_with_dfs(v_iter, h_iter, &new_feature);
+                if(return_features_list.number_of_features < MAX_FEATURES_PER_TOF_ARRAY)
+                {
+                    //add new node 
+                    return_features_list.features[return_features_list.number_of_features] = new_feature;
+                    return_features_list.number_of_nodes_in_feature[return_features_list.number_of_features] = number_of_nodes_in_feature;
+                    return_features_list.number_of_features++;
+                }
+                else
+                {
+                    //if new feature is larger than the smallest current feature then replace feature on list
+                    uint8_t min_feature = 0;
+                    for(uint8_t list_iter = 0; list_iter < return_features_list.number_of_features; list_iter++)
+                    {
+                        if(return_features_list.number_of_nodes_in_feature[list_iter] < return_features_list.number_of_nodes_in_feature[min_feature])
+                        {
+                            min_feature = list_iter;
+                        }
+                    }
+                    if(number_of_nodes_in_feature > return_features_list.number_of_nodes_in_feature[min_feature])
+                    {
+                        return_features_list.features[min_feature] = new_feature;
+                        return_features_list.number_of_nodes_in_feature[min_feature] = number_of_nodes_in_feature;
+                    }
+                }
+                
+            }
+        }
+    }
     //3. Combine islands that are likely the same island, then order islands from largest to smallest in terms of number of cells.
     //4. determine center of mass of each island, xyz dimensions, as well as their orientation vs the robot.
     return return_features_list;
